e6416fa9df15945c520205d6a63878239b0ad77f
Marcos Luciano <prmarcosluciano2013@gmai.com>
2024-07-20 21:46:51 -0300

    update 2

--- aula-java01/src/MinhaClasse.java
+++ aula-java01/src/MinhaClasse.java
@@ -1,8 +1,17 @@
 public class MinhaClasse {
 
 public static void main(String[] args) {
-    System.out.print ("Olá turma, Sejam bem vindos! ");
+    String primeiroNome = "Marcos";
+    String segundoNome = "Rodrigues";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
 
 }
-   
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do Metodo " + primeiroNome.concat(" ").concat(segundoNome);
+
+}
+
 }
